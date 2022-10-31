8a6154962ab89448a4c1e0abf2325928e1bb2adc
Aasawari Saprey <71440730+aasawari-saprey@users.noreply.github.com>
2022-11-01 01:59:55 +0530

    Update project.js

--- project.js
+++ project.js
@@ -1,7 +1,7 @@
 const express=require ('express');
 const router=express.Router();
 const bodyParser=require('body-parser')
-const db=require('../db');
+const db=require('./db');
 
 router.use(bodyParser.json({urlencoded:true}));
 
